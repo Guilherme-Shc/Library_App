8508fde2c15500a2c99351f80f18faccfaeb0b3f
Guilherme-Shc <antguischcor@gmail.com>
2024-04-15 21:18:05 -0300

    first commit

---
+++ Library.Communication/Enums/BookGenre.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Communication.Enums
+{
+    public enum BookGenre
+    {
+        Horror = 0,
+        Action = 1,
+        Romance = 2,
+        Drama = 3,
+        Adventure = 4
+    }
+}
---
+++ Library.Communication/Responses/ResponseBookJson.cs
@@ -0,0 +1,14 @@
+﻿using Library.Communication.Enums;
+
+namespace Library.Communication.Responses
+{
+    public class ResponseBookJson
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public BookGenre GenreType { get; set; }
+        public int Value { get; set; }
+        public int Quantity { get; set; }
+    }
+}
